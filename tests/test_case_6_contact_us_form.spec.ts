e86ab76b31a9c8f422f7325b38b4599543acf867
Antonijela Gasic <antonijela_gasic@epam.com>
2025-02-27 23:18:42 +0100

    new test case for contact us form

---
+++ tests/test_case_6_contact_us_form.spec.ts
@@ -0,0 +1,27 @@
+import {test, expect} from '@playwright/test'
+import { Homepage } from '../pages/homepage'
+import { ContactUs } from '../pages/contactUsPage'
+import * as testData from '../test_data/user_ana.json'
+import path from 'path'
+
+test('verify sending contact us form', async({page}) => {
+    const homepage = new Homepage(page)
+    const contactUsPage = new ContactUs(page)
+    const user_ana = testData.user
+    const filePath = path.resolve(__dirname, '../test_data/test-image.jpg')
+
+    await page.goto('/')
+    expect(await homepage.logo.isVisible()).toBeTruthy()
+    expect(await homepage.navBar.isVisible()).toBeTruthy()
+    await homepage.contactUsOption.click()
+
+    expect (await contactUsPage.getInTouchTitle.isVisible()).toBeTruthy()
+    await contactUsPage.nameField.fill(user_ana.name)
+    await contactUsPage.emailField.fill(user_ana.email)
+    await contactUsPage.subjectField.fill('subject title')
+    await contactUsPage.messageField.fill('some message in the field')
+    await contactUsPage.uploadButton.setInputFiles(filePath)
+    await contactUsPage.submitButton.click()
+
+git a
+})
