5ba4dbc9ed32ab239311cb7ffdd9decae1250c16
Antonijela Gasic <antonijela_gasic@epam.com>
2025-03-05 08:38:48 +0100

    Added test case for verifying test cases page and steps of the first test case

---
+++ tests/test_case_7_verify_test_cases_page.spec.ts
@@ -0,0 +1,28 @@
+import {test, expect} from '@playwright/test'
+import { Homepage } from '../pages/homepage'
+import { TestCases } from '../pages/testCasesPage'
+import * as testCaseSteps from '../test_data/test_case_1_steps.json'
+
+test('verify steps of first test case', async ({page}) => {
+    const homepage = new Homepage(page)
+    const testCasesPage = new TestCases(page)
+    const test_case_steps = testCaseSteps
+
+    await page.goto('/')
+    expect(await homepage.logo.isVisible()).toBeTruthy()
+    expect(await homepage.navBar.isVisible()).toBeTruthy()
+    await homepage.testCaseButton.click()
+
+    expect(await testCasesPage.title.isVisible()).toBeTruthy()
+    
+    const list = await testCasesPage.testCasesList.count()
+    expect(list).toBe(26)
+
+    await testCasesPage.firstTestCase.click()
+
+    for (let i = 1; i <= Object.keys(test_case_steps).length; i++) {
+        const stepText = test_case_steps[`step${i}`]
+
+        const stepLocator = page.locator(`#collapse1 .list-group-item:has-text("${stepText}")`)
+    }
+})
